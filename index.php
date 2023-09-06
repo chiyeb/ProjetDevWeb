4d55bd44e923cda24b50341df30d258acab35b4e
chihebbradai <chihebbradai@gmail.Com>
2023-09-06 16:51:05 +0200

    1e commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mon Blog</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+<header>
+    <h1>Y</h1>
+    <nav>
+        <ul>
+            <li><a href="#">Accueil </a></li>
+            <li><a href="#">Posts</a></li>
+            <li><a href="#">À propos</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </nav>
+</header>
+
+<main>
+    <article>
+        <h2>Titre de l'article</h2>
+        <p>Date de publication : 6 septembre 2023</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in justo ut ipsum volutpat tempus. Nullam congue sem nec eros rhoncus, nec vulputate lorem hendrerit.</p>
+        <a href="#">Lire la suite...</a>
+    </article>
+
+    <article>
+        <h2>Un autre article</h2>
+        <p>Date de publication : 7 septembre 2023</p>
+        <p>Integer auctor bibendum metus, sit amet malesuada nisi bibendum nec. Sed a urna non quam laoreet dignissim.</p>
+        <a href="#">Lire la suite...</a>
+    </article>
+</main>
+
+<footer>
+    <p>&copy; 2023 Mon Blog</p>
+</footer>
+</body>
+</html>
