1ae29c1c19cb96c6cedd84d8d6afcdc2b7ba0bd4
Mathis <mathis.delmas@etu.univ-amu.fr>
2023-09-14 15:07:03 +0200

    header page principal

--- index.php
+++ index.php
@@ -1,42 +1,29 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="fr">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mon Blog</title>
     <link rel="stylesheet" href="styles.css">
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
 </head>
 <body>
 <header>
-    <h1>Y</h1>
+    <div class="logo">Y</div>
+    <div class="middle-buttons">
+        <a class="btn-menu" href="accueil.php">Accueil</a>
+        <a class="btn-menu" href="pour-toi.php">Pour toi</a>
+    </div>
     <nav>
-        <ul>
-            <li><a href="#">Accueil </a></li>
-            <li><a href="#">Posts</a></li>
-            <li><a href="#">À propos</a></li>
-            <li><a href="#">Contact</a></li>
-        </ul>
+        <a class="btn" href="page-login.html">Login</a>
+        <a class="btn" href="page-register.html">Register</a>
     </nav>
+
 </header>
 
-<main>
-    <article>
-        <h2>Titre de l'article</h2>
-        <p>Date de publication : 6 septembre 2023</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in justo ut ipsum volutpat tempus. Nullam congue sem nec eros rhoncus, nec vulputate lorem hendrerit.</p>
-        <a href="#">Lire la suite...</a>
-    </article>
 
-    <article>
-        <h2>Un autre article</h2>
-        <p>Date de publication : 7 septembre 2023</p>
-        <p>Integer auctor bibendum metus, sit amet malesuada nisi bibendum nec. Sed a urna non quam laoreet dignissim.</p>
-        <a href="#">Lire la suite...</a>
-    </article>
-</main>
-
-<footer>
-    <p>&copy; 2023 Mon Blog</p>
-</footer>
 </body>
 </html>
+
+
